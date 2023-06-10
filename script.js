7713a459f9c92405b08dc34ec2bd8377f06b8a0c
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-10 13:15:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+// Function to play the sound based on the key press
+function playSound(event) {
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  if (!key) return;
+
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  audio.currentTime = 0; // Rewind the audio to the start
+  audio.play();
+
+  key.classList.add('playing');
+}
+
+// Function to remove the playing class when the transition ends
+function removePlayingClass(event) {
+  if (event.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+// Attach event listeners
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
+window.addEventListener('keydown', playSound);
