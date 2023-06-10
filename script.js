75d91ab36eda7f62071f7b4408f059c81c926aa1
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-10 13:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,16 +11,16 @@ const audioPaths = {
   76: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // L
 };
 
-// Function to play the sound based on the key press
+
 function playSound(event) {
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
   if (key) {
-    // Play sound logic here
+ 
     const audio = new Audio(audioPaths[event.keyCode]);
     audio.play();
 
-    // Add playing effect
+  
     key.classList.add('playing');
     setTimeout(() => {
       key.classList.remove('playing');
