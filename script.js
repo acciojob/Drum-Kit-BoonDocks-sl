cca1de25c1b4613f976d3ed0db561b31efbdefbd
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-10 13:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //your JS code here. If required.
 const audioPaths = {
-  65: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // A
-  83: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // S
-  68: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // D
-  70: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // F
-  71: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // G
-  72: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // H
-  74: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // J
-  75: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // K
-  76: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // L
+  65: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // A
+  83: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // S
+  68: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // D
+  70: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // F
+  71: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // G
+  72: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // H
+  74: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // J
+  75: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // K
+  76: 'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3', // L
 };
 
 
