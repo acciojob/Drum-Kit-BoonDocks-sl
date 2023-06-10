40e04bd7477f90366da27c18ed3c98a88e367643
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-10 13:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 //your JS code here. If required.
+const audioPaths = {
+  65: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // A
+  83: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // S
+  68: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // D
+  70: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // F
+  71: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // G
+  72: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // H
+  74: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // J
+  75: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // K
+  76: 'https://pixabay.com/sound-effects/2021/06/09/audio_60bfb73ef9ef7.mp3', // L
+};
+
 // Function to play the sound based on the key press
 function playSound(event) {
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if (!key) return;
 
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  audio.currentTime = 0; // Rewind the audio to the start
-  audio.play();
+  if (key) {
+    // Play sound logic here
+    const audio = new Audio(audioPaths[event.keyCode]);
+    audio.play();
 
-  key.classList.add('playing');
+    // Add playing effect
+    key.classList.add('playing');
+    setTimeout(() => {
+      key.classList.remove('playing');
+    }, 100);
+  }
 }
 
-// Function to remove the playing class when the transition ends
-function removePlayingClass(event) {
-  if (event.propertyName !== 'transform') return;
-  this.classList.remove('playing');
-}
-
-// Attach event listeners
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removePlayingClass));
+// Attach event listener
 window.addEventListener('keydown', playSound);
+
